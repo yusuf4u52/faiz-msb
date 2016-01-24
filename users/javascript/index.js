07beb327b05ba73f23a06fe5a48cad23d80c4d35
Ismail Sidhpuri <ismail.sidhpuri@iris.co.uk>
2016-01-24 16:55:21 +0530

    bug fixes wrong format for input date semicolon

--- users/javascript/index.js
+++ users/javascript/index.js
@@ -4,7 +4,7 @@ for (var i = 0; i < els.length; i++) {
     var el = els[i];
     var prop = (el.tagName == 'INPUT') ? 'value' : 'innerText';
     var greg = el[prop];
-    el[prop] = moment(greg, 'YYYY-MM-DD').add(1,'day').format('iD iMMMM iYYYY');
+    el[prop] = moment(greg, 'YYYY-MM-DD').add(1,'day').format('iYYYY-iM-iD');
     //NOTE: Moment.js hijri plugin calculates according to avaam date. So adding a day when converting to hijri.
     //In the same way if, we have to convert hijri to greg, we should subtract a day i.e. add -1 day.
 }
