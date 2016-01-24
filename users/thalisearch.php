cac1d5a82ec0060ea36891b5fcd8f7a3af63b9fa
Ismail Sidhpuri <ismail.sidhpuri@iris.co.uk>
2016-01-24 17:52:41 +0530

    added mustafamnr@gmail.com to authentication

--- users/thalisearch.php
+++ users/thalisearch.php
@@ -2,7 +2,7 @@
 include('connection.php');
 session_start();
 
-if (!is_null($_SESSION['fromLogin']) && in_array($_SESSION['email'], array('yusuf4u52@gmail.com','tzabuawala@gmail.com','bscalcuttawala@gmail.com'))) {
+if (!is_null($_SESSION['fromLogin']) && in_array($_SESSION['email'], array('yusuf4u52@gmail.com','tzabuawala@gmail.com','bscalcuttawala@gmail.com','mustafamnr@gmail.com'))) {
  
 }
 else
