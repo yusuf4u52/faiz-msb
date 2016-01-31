4a3e989178463c1786ca989785f2c1642d433740
Murtaza Haveliwala <murtaza.haveliwala@synerzip.com>
2016-01-31 12:35:40 +0530

    admin crud

---
+++ admin/application/controllers/Examples.php
@@ -0,0 +1,55 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Examples extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		session_start();
+		if (!is_null($_SESSION['fromLogin']) && in_array($_SESSION['email'], array('yusuf4u52@gmail.com','tzabuawala@gmail.com','bscalcuttawala@gmail.com','mustafamnr@gmail.com')))
+		{
+
+		}else
+		 header("Location: ../../../users/login.php");
+
+		$this->load->database();
+		$this->load->helper('url');
+
+		$this->load->library('grocery_CRUD');
+	}
+
+	public function _example_output($output = null)
+	{
+		$this->load->view('example.php',$output);
+	}
+
+	public function offices()
+	{
+		$output = $this->grocery_crud->render();
+
+		$this->_example_output($output);
+	}
+
+	function faiz()
+{
+    $crud = new grocery_CRUD();
+    $crud->set_theme('datatables');
+    $crud->set_table('thalilist');
+
+    $output = $crud->render();
+ 
+    $this->_example_output($output);
+}
+
+	public function index()
+	{
+		$this->_example_output((object)array('output' => '' , 'js_files' => array() , 'css_files' => array()));
+	}
+
+		
+	public function valueToEuro($value, $row)
+	{
+		return $value.' &euro;';
+	}
+			
+}
