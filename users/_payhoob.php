806529b6433c2753e45479c3912e68a779a65ba5
Yusuf Rampurawala <yusuf4u52@gmail.com>
2016-02-02 17:48:08 +0530

    Modifying the payhoob to first update the receipt table and than the main thali list. Also capturing any db related errors.

--- users/_payhoob.php
+++ users/_payhoob.php
@@ -4,9 +4,11 @@ include('adminsession.php');
 
 if($_POST)
 {
-  $sql = "UPDATE thalilist set Paid = Paid + " . $_POST["receipt_amount"] . "   WHERE thali = ". $_POST['receipt_thali'];
-  $update = mysqli_query($link, $sql);
-  $sql = "INSERT INTO receipts (`Receipt No`, `Thali No`, `Amount`, `Date`) VALUES (" . $_POST["receipt_number"] . "," . $_POST['receipt_thali'] . "," . $_POST["receipt_amount"] . ", '" . $_POST['receipt_date'] . "')";
-  $insert = mysqli_query($link, $sql);
+  
+  $sql = "INSERT INTO receipts (`Receipt No`, `Thali No`, `Amount`, `Date`) VALUES ('" . $_POST['receipt_number'] . "','" . $_POST['receipt_thali'] . "','" . $_POST['receipt_amount'] . "', '" . $_POST['receipt_date'] . "')";
+  $insert = mysqli_query($link, $sql) or die(mysqli_error($link));
+  $sql = "UPDATE thalilist set Paid = Paid + '" . $_POST['receipt_amount'] . "' WHERE thali = '" . $_POST['receipt_thali']."'";
+  $update = mysqli_query($link, $sql) or die(mysqli_error($link));
+  
   echo "success";
 }
