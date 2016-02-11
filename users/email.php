9b15453d896f38e560eb25100c79dee2ef8baf27
Yusuf Rampurawala <yusuf4u52@gmail.com>
2016-02-11 15:03:50 +0530

    Remove duplicates from CR NR

--- users/email.php
+++ users/email.php
@@ -49,6 +49,10 @@ $count=mysqli_num_rows($result);
 
 $msgvar .= "\n Count \n $count ";
 
+// Remove Duplicate 
+
+$msgvar = implode("\n",array_unique(explode("\n", $msgvar)));
+
 $myfile = fopen("requestarchive.txt", "a") or die("Unable to open file!");
 $txt= date('d/m/Y')."\n".$msgvar."\n";
 fwrite($myfile, $txt);
