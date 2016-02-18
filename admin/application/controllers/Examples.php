ab6f89a1616ec386f149ab7035dd1336cabb0633
Yusuf Rampurawala <yusuf4u52@gmail.com>
2016-02-18 16:28:33 +0530

    unset edit delete

--- admin/application/controllers/Examples.php
+++ admin/application/controllers/Examples.php
@@ -53,6 +53,8 @@ function receipts()
     $crud = new grocery_CRUD();
     $crud->set_theme('datatables');
     $crud->set_table('receipts');
+    $crud->unset_edit();
+    $crud->unset_delete();
     $crud->columns('Receipt_No','Thali_No','Amount','Date');
 
     $output = $crud->render();
