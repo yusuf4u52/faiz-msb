b9bd62f6cc30183bad1ce295eda7d7bfafa8a9a0
IsmailS <ismailsidhpuri@gmail.com>
2016-02-25 22:06:39 +0530

    started work on hoob history for students

---
+++ users/hoobHistory.php
@@ -0,0 +1,40 @@
+<?php
+include('_authcheck.php');
+include('connection.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <?php include('_head.php'); ?>
+  </head>
+  <body>
+    <?php include('_nav.php'); ?>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <table class="table table-striped table-hover">
+          <tr>
+          <td><b>Receipt No</b></td>
+          <td><b>Amount</b></td>
+          <td><b>Date</b></td>
+          </tr>
+          <?php
+          $query = "SELECT * FROM receipts r, thalilist t WHERE r.Thali_No = t.Thali and t.Email_ID ='".$_SESSION['email']."'";
+          $result = mysqli_query($link,$query);
+          while($row = mysqli_fetch_assoc($result)){ 
+          foreach($row AS $key => $value) { $row[$key] = stripslashes($value); } 
+            echo "<tr>";  
+            echo "<td>" . nl2br( $row['Receipt_No']) . "</td>";  
+            echo "<td>" . nl2br( $row['Amount']) . "</td>";  
+            echo "<td>" . nl2br( $row['Date']) . "</td>";  
+            echo "</tr>"; 
+          } 
+          ?>
+          </table>
+        </div>
+      </div>
+    </div>
+    <?php include('_bottomJS.php'); ?>
+  </body>
+</html>
+
