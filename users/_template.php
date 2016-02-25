a32f34293907b63ba350575bc4ff845f74fc656c
IsmailS <ismailsidhpuri@gmail.com>
2016-02-25 20:53:42 +0530

    Being DRY (https://en.wikipedia.org/wiki/Don%27t_repeat_yourself)

---
+++ users/_template.php
@@ -0,0 +1,20 @@
+<?php
+include('connection.php');
+include('_authcheck.php');
+//TODO:Do your DB operations and other processing here
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <?php include('_head.php'); ?>
+  <!--TODO: add extra includes here if required only on this page -->
+  </head>
+  <body>
+    <?php include('_nav.php'); ?>
+    <div class="container">
+      <!-- TODO - All page specific HTML goes here -->
+    </div>
+    <?php include('_bottomJS.php'); ?>
+    <!-- TODO - Any javascript include that you require only on this page will be added here -->
+  </body>
+</html>
