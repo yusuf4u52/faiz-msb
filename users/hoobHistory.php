f6c0c14b07d8cedef379bcf5143eda468c7ea1fe
IsmailS <ismailsidhpuri@gmail.com>
2016-02-26 21:59:28 +0530

    hoob history for user #11

--- users/hoobHistory.php
+++ users/hoobHistory.php
@@ -25,17 +25,17 @@ include('connection.php');
           echo $query;
           while($row = $result_tables->fetch_array(MYSQLI_NUM)){ 
             if (strpos($row[0], 'receipts_') !== false) {
-              $query .= "SELECT * FROM ".$row[0]." r, thalilist t WHERE r.Thali_No = t.Thali and t.Email_ID ='".$_SESSION['email']."' UNION ";
+              $query .= "SELECT r.* FROM ".$row[0]." r, thalilist t WHERE r.Thali_No = t.Thali and t.Email_ID ='".$_SESSION['email']."' UNION ";
             }
           }
-          $query .= "SELECT * FROM receipts r, thalilist t WHERE r.Thali_No = t.Thali and t.Email_ID ='".$_SESSION['email']."'";
+          $query .= "SELECT r.* FROM receipts r, thalilist t WHERE r.Thali_No = t.Thali and t.Email_ID ='".$_SESSION['email']."' ORDER BY Date DESC";
           $result = mysqli_query($link,$query);
           while($row = mysqli_fetch_assoc($result)){ 
           foreach($row AS $key => $value) { $row[$key] = stripslashes($value); } 
             echo "<tr>";  
             echo "<td>" . nl2br( $row['Receipt_No']) . "</td>";  
             echo "<td>" . nl2br( $row['Amount']) . "</td>";  
-            echo "<td>" . nl2br( $row['Date']) . "</td>";  
+            echo "<td class=\"hijridate\">" . nl2br( $row['Date']) . "</td>";  
             echo "</tr>"; 
           } 
           ?>
