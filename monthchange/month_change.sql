91f28251c3c1fa290c16fe72c81c42c345a04e88
Yusuf Rampurawala <yusuf4u52@gmail.com>
2016-02-29 19:49:30 +0530

    Expense report

--- monthchange/month_change.sql
+++ monthchange/month_change.sql
@@ -12,11 +12,13 @@ UPDATE thalilist SET TranspFee = 250 where Active = 1 AND Transporter != 'Pick U
 UPDATE thalilist SET TranspFee = 350 where Active = 1 AND Transporter != 'Pick Up' AND Thali IN (9,410,602);
 
 -- Kothrud
-UPDATE thalilist SET TranspFee = 500 where Active = 1 AND Transporter != 'Pick Up' AND Thali IN (202, 593, 594, 595, 596, 615, 600, 629);
+UPDATE thalilist SET TranspFee = 500 where Active = 1 AND Transporter != 'Pick Up' AND Thali IN (202,593,594,595,596,615,600,629);
 
 -- Saif Colony
 UPDATE thalilist SET TranspFee = 400 where Active = 1 AND Transporter != 'Pick Up' AND Thali IN (237,520);
 
 -- Hoob adjust
-UPDATE thalilist SET Dues = 1400 where Active = 1 AND Thali IN (553);
+UPDATE thalilist SET Dues = 1400 where Active = 1 AND Thali IN (553,548,618,619,620);
 UPDATE thalilist SET Dues = 1000 where Active = 1 AND Thali IN (41);
+UPDATE thalilist SET Dues = 1100 where Active = 1 AND Thali IN (112,598);
+UPDATE thalilist SET Dues = 1500 where Active = 1 AND Thali IN (431);
