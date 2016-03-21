492c1716cb8f6e7761327455420bfe6cd8b4de5d
Yusuf Rampurawala <yusuf4u52@gmail.com>
2016-03-21 17:30:55 +0530

    icon at login page

--- users/login.php
+++ users/login.php
@@ -5,29 +5,8 @@
 
 <html lang="en">
   <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+      <?php include('_head.php'); ?>
 
-    <meta charset="utf-8" />
-
-    <title>Faiz ul Mawaid il Burhaniyah (Poona Students)</title>
-
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-
-    <link rel="stylesheet" href="./src/bootstrap.css" media="screen" />
-
-    <link rel="stylesheet" href="./src/custom.min.css" />
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-
-    <!--[if lt IE 9]>
-
-    <script src="javascript/html5shiv-3.7.0.min.js"></script>
-
-    <script src="javascript/respond-1.4.2.min.js"></script>
-
-    <![endif]-->
   </head>
 
   <body>
