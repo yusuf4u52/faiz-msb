9eb90dc56fecf05686bf61956ab1f7192bc900e6
Yusuf Rampurawala <yusuf4u52@gmail.com>
2016-05-28 22:22:45 +0530

    24k added

--- users/selectyearlyhub_action.php
+++ users/selectyearlyhub_action.php
@@ -6,8 +6,9 @@ if(isset($_GET['option']))
 {
 	$huboptions = array(
 						1 => '21000',
-						2 => '27000',
-						3 => '53000'
+						2 => '24000',
+						3 => '27000',
+						4 => '53000'
 							);
 	$hub_amount = $huboptions[$_GET['option']];
 	$update = mysqli_query($link,"UPDATE thalilist set yearly_hub='".$hub_amount."' WHERE Email_id = '".$_SESSION['email']."'");
