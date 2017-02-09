8aea8c45c2a7f1713842d23703ca13c5a0cdc1f6
Yusuf Rampurawala <yusuf4u52@gmail.com>
2017-02-09 12:44:07 +0530

    close for expense view

--- users/expenses_new.php
+++ users/expenses_new.php
@@ -36,6 +36,7 @@ foreach ($months as $key => $month) {
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
         <h4 class="modal-title">Expense Breakdown</h4>
       </div>
       <div class="modal-body">
