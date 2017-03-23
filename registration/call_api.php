0c75ac7c8babad6a16a7662fa1652303da851fd4
Yusuf Rampurawala <yusuf4u52@gmail.com>
2017-03-23 13:08:35 +0530

    curl may be not working on hostinger reverting change

--- registration/call_api.php
+++ registration/call_api.php
@@ -4,15 +4,7 @@
 
 function CallAPI($itsid)
 {	
-	$url = "http://13.82.146.207:8085/user?itsid=".$itsid."";
-    $curl = curl_init();
-
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-
-    $result = curl_exec($curl);
-
-    curl_close($curl);
-    return $result;
+	$result = file_get_contents("http://13.82.146.207:8085/user?itsid=".$itsid);
+    return json_decode($result,true);
 }
 ?>
