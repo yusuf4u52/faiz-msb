d651e046746e98f6145b0976daf95c40bfaf6a57
Yusuf Rampurawala <yusuf4u52@gmail.com>
2018-04-08 12:58:06 +0530

    update all ITS data script for admin

--- registration/call_api.php
+++ registration/call_api.php
@@ -15,4 +15,31 @@ function CallAPI($itsid,$header,$its_url)
 	$result = file_get_contents($its_url.$itsid, false, $context);
 	return json_decode($result,true);
 }
+
+function CallAPIForAll()
+{
+	include('../users/connection.php');
+	require '../sms/_credentials.php';
+	$query="SELECT ITS_No FROM thalilist";
+	$result = mysqli_query($link,$query);
+	if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+	    while($row = mysqli_fetch_assoc($result)) {
+	        $its = $row['ITS_No'];
+	        if (!empty($its)) {
+	            $datafromits = CallAPI($its,$header,$its_url);
+	        }
+	        if(!empty($datafromits)) {
+		        $datafromits_name = $datafromits['name'];
+			    $datafromits_jamaat = $datafromits['jamaat'];
+				$update="update thalilist set NAME='".$datafromits_name."' AND WATAN='".$datafromits_jamaat."' where ITS_No='".$its."' ";
+				mysqli_query($link,$update);
+			}
+
+	    }
+	} else {
+	    echo "0 results";
+	}
+	mysqli_close($link);
+}
 ?>
---
+++ users/admin_scripts.php
@@ -0,0 +1,22 @@
+<?php
+include('_authCheck.php');
+include('adminsession.php');
+include('_bottomJS.php');
+include('../registration/call_api.php');
+
+if (isset($_GET['update'])) {
+    CallAPIForAll();
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Scripts for Admin</title>
+	<?php include('_head.php'); ?>
+</head>
+<body>
+	<a href="/users/integrity_check.php" class="btn btn-info" role="button">Receipts Integrity</a>
+	<a href="/monthchange/month_change.php" class="btn btn-info" role="button">Year Change</a>
+	<a href="admin_scripts.php?update=true" class="btn btn-info" role="button">Update From ITS</a>
+</body>
+</html>
