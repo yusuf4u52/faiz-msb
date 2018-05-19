544a10e9c0b34e0c5f71411f2b7a4dfda17d2c1d
Murtaza Raja <murtraja@gmail.com>
2018-05-19 16:36:07 +0530

    adding pattern for input validation for father's no.

--- users/update_details.php
+++ users/update_details.php
@@ -137,7 +137,7 @@ mysqli_query($link,"INSERT INTO change_table (`Thali`, `Operation`, `Date`) VALU
                   <div class="form-group">
                     <label for="inputContact" class="col-lg-2 control-label">Fathers No.</label>
                     <div class="col-lg-10">
-                      <input type="text" class="form-control" id="inputContact" required='required' name="fathercontact" value='<?php echo $fathersNo;?>'>
+                      <input type="text" pattern="\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1) (\d{8,14})" class="form-control" id="inputContact" required='required' name="fathercontact" value='<?php echo $fathersNo;?>'>
                     </div>
                   </div>
 
