c79297218f52259f2192fc9ed0a4680273c79ae4
Yusuf Rampurawala <yusuf4u52@gmail.com>
2019-04-06 12:46:04 +0530

    users and home aligned

--- users/_head.php
+++ users/_head.php
@@ -3,10 +3,16 @@
 <title>Faiz ul Mawaid il Burhaniyah (Poona Students)</title>
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-<link rel="stylesheet" href="/users/src/bootstrap.css" media="screen" />
+<link rel="stylesheet" href="/styles/css/bootstrap-theme-white-plum/bootstrap.min.css"/>
 <link rel="stylesheet" href="/users/src/custom.min.css" />
 <link rel="stylesheet" href="/users/src/jquery.dynatable.css" />
 <link rel="icon" href="/users/images/icon.png">
+<style type="text/css">
+	.well {
+		background-color: #fff;
+		color: #000;
+	}
+</style>
 <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!--[if lt IE 9]>
 <script src="javascript/html5shiv-3.7.0.min.js"></script>
