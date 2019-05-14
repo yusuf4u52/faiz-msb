af4cb7150dd33bc1f051a59bfe819e68de0cadaf
Murtaza Raja <murtraja@gmail.com>
2019-05-14 20:33:14 +0530

    typo fix () => []

--- users/adminsession.php
+++ users/adminsession.php
@@ -3,7 +3,7 @@
 
 if (!is_null($_SESSION['fromLogin']) && in_array($_SESSION['email'], array('mesaifee52@gmail.com','husainpoonawala1995@gmail.com','nationalminerals52@gmail.com','murtaza52@gmail.com','yusuf4u52@gmail.com','tzabuawala@gmail.com','hzfshakir199@gmail.com','murtaza.sh@gmail.com','mustafamnr@gmail.com', 'ismailsidhpuri@gmail.com'))) {
  
-} elseif (!empty($_POST('mobile'))) {
+} elseif (!empty($_POST['mobile'])) {
 	// set email based on mobile number
   $sql = mysqli_query($link,"SELECT * from users where mobile='".$_POST('mobile')."'");
   $row = mysqli_fetch_row($sql);
