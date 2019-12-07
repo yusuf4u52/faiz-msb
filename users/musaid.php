219580151e2396fea824ebd94e7ba254729f6bd5
Yusuf Rampurawala <yusuf4u52@gmail.com>
2019-12-07 15:02:26 +0530

    On reject of new thali, data will not be deleted. Musaid page now saves comment and commited date.

--- users/musaid.php
+++ users/musaid.php
@@ -1,7 +1,13 @@
 <?php
 include('_authCheck.php');
-// role must be greater than musaid to see this page.
-
+if ($_POST) { 
+	if(!empty($_POST['comment']) || !empty($_POST['date'])) {
+    	mysqli_query($link,"INSERT INTO `hub_commitment` (`thali`, `comments`, `commit_date`) VALUES ('".$_POST['Thali']."', '".$_POST['comment']."', '".$_POST['date']."')") or die(mysqli_error($link));
+	}
+header("Location: musaid.php");
+exit;
+}
+                          
 ?>
 <!DOCTYPE html>
 <html>
@@ -11,51 +17,12 @@ include('_authCheck.php');
 	<?php include('_bottomJS.php'); ?>
 	<script>
 	  $( function() {
-	    $( ".datepicker" ).datepicker({ dateFormat: 'dd/mm/yy' });
+	    $( ".datepicker" ).datepicker({ dateFormat: 'yy-mm-dd' });
 	  } );
   	</script>
 </head>
 <body>
 	<?php include('_nav.php'); ?>
-
-	<!-- Modal Window to add friends -->
-	<div class="modal" id="modal">
-	  <div class="modal-dialog" role="document">
-	    <div class="modal-content">
-	      <form method="post" action="event_add_friend.php">
-	      <div class="modal-header">
-	        <h5 class="modal-title">Add a friend</h5>
-	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-	          <span aria-hidden="true">&times;</span>
-	        </button>
-	      </div>
-	      <div class="modal-body">
-			  <fieldset>
-			    <div class="form-group">
-			      <label>ITS</label>
-			      <input type="text" class="form-control" name="its" required placeholder="Enter ITS" pattern="[0-9]{8}">
-			    </div>
-			    <div class="form-group">
-			      <label>Full Name</label>
-			      <input type="text" class="form-control" name="name" required placeholder="Enter Full Name">
-			    </div>
-			    <div class="form-group">
-			      <label>Mobile</label>
-			      <input type="text" class="form-control" name="mobile" required placeholder="Enter Mobile" pattern="[0-9]{10}">
-			    </div>
-			    <input type="hidden" name="reference_id" id="add_friend_refid">
-			    <input type="hidden" name="eventid" id="add_friend_eventid">
-			  </fieldset>
-	      </div>
-	      <div class="modal-footer">
-	        <button type="submit" class="btn btn-primary">Save changes</button>
-	        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-	      </div>
-	      </form>
-	    </div>
-	  </div>
-	</div>
-
 	<div class="container">
 		<table class="table table-hover">
 		  <thead>
@@ -67,53 +34,38 @@ include('_authCheck.php');
 		      <th scope="col">Pending</th>
 		      <th scope="col">Action</th>
 		      <th scope="col">Commited Date</th>
+		      <th scope="col">Comments</th>
+		      <th scope="col">Action</th>
 		    </tr>
 		  </thead>
 		  <tbody>
 				<?php
-				$result=mysqli_query($link,"SELECT Thali, Name, contact, yearly_hub, total_pending FROM thalilist where musaid='".$_SESSION['email']."'");
+				$result=mysqli_query($link,"SELECT Thali, Name, contact, yearly_hub, total_pending, WhatsApp FROM thalilist where total_pending > 0 and musaid='".$_SESSION['email']."'");
+
 		      while($values = mysqli_fetch_assoc($result))
 		      {
+		      	$last_comments=mysqli_query($link,"SELECT * FROM hub_commitment where thali='".$values['Thali']."'");
+		      	 	$all_data = mysqli_fetch_all($last_comments);
+				    $all_comments=array_column($all_data,2);
+
 		    ?>
-		    <tr>
-		      <td><?php echo $values['Thali']; ?></td>
-		      <td><?php echo $values['Name']; ?></td>
-		      <td><?php echo $values['contact']; ?></td>
-		      <td><?php echo $values['yearly_hub']; ?></td>
-		      <td><?php echo $values['total_pending']; ?></td>
-		      <td><a target="_blank" href="https://wa.me/91<?php echo $values['contact']; ?>?text=">WhatsApp</a></td>
-		      <td><input type="text" class="datepicker"></td>
-		    </tr>
+		    <form method="post">
+			    <tr>
+			      <input type='hidden' value='<?php echo $values['Thali']; ?>' name='Thali'>
+			      <td><?php echo $values['Thali']; ?></td>
+			      <td><?php echo $values['Name']; ?></td>
+			      <td><?php echo $values['contact']; ?></td>
+			      <td><?php echo $values['yearly_hub']; ?></td>
+			      <td><?php echo $values['total_pending']; ?></td>
+			      <td><a target="_blank" href="https://wa.me/91<?php echo explode(" ", $values['WhatsApp'])[1]; ?>?text=Salaam Bhai, Tamari FMB ni hub pending che. Tame kivar tak ada karso?">WhatsApp</a></td>
+			      <td><input type="text" name="date" class="datepicker" autocomplete="off"></td>
+			      <td><?php echo "<pre>".implode(",\n",$all_comments)."</pre>"; ?><textarea name="comment" class="form-control" rows="3"></textarea></td>
+			      <td><input type='submit' value="Save"></td>
+			    </tr>
+			</form>
 		    <?php } ?>
 		  </tbody>
 		</table>
 	</div>
 </body>
-<script>
-$(document).ready(function(){
-    $(".btn-response").click(function(){
-    	$(".action-" + $(this).data("eventid")).attr("disabled", true);
-    	$.ajaxSetup({
-		    beforeSend: function(xhr) {
-		        xhr.setRequestHeader('User-Agent', 'Googlebot/2.1 (+http://www.google.com/bot.html)');
-		    }
-		});
-        $.post("event_response.php",
-        {
-          Response: $(this).data("response"),
-          Thaliid: $(this).data("thaliid"),
-          Eventid: $(this).data("eventid")
-        },
-        function(data,status){
-            alert("Response Submitted Successfully");
-        });
-    });
-
-    $(".add_friend").click(function(){
-    	$('#add_friend_refid').val($(this).data('thaliid'));
-    	$('#add_friend_eventid').val($(this).data('eventid'));
-    	$("#modal").modal();
-    });
-});
-</script>
 </html>
--- users/reject.php
+++ users/reject.php
@@ -4,7 +4,7 @@ include('connection.php');
 include('adminsession.php');
 require '_sendMail.php';
 
-mysqli_query($link,"DELETE from thalilist WHERE Email_id = '".$_POST['email']."'") or die(mysqli_error($link));
+mysqli_query($link,"Update thalilist set Active='2' WHERE Email_id = '".$_POST['email']."'") or die(mysqli_error($link));
 
 $msgvar = 'Salam %name%,<br><br>Your thaali has not been activated as we are not currently able to deliver at your address. For any queries please mail us at <b>help@faizstudents.com.</b><br><br>Regards,<br>Faiz Team';
 
