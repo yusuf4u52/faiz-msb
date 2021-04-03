556b58e8c678298bbd5f8acab6046557d7ecf8cd
Yusuf Rampurawala <yusuf4u52@gmail.com>
2021-04-03 16:23:19 +0530

    yearly hub 1442

--- users/selectyearlyhub.php
+++ users/selectyearlyhub.php
@@ -2,58 +2,58 @@
 include('connection.php');
 include('_authCheck.php');
 
-$query="SELECT yearly_hub FROM thalilist where Email_id = '".$_SESSION['email']."'";
+$query = "SELECT yearly_hub FROM thalilist where Email_id = '" . $_SESSION['email'] . "'";
 
-$values = mysqli_fetch_assoc(mysqli_query($link,$query));
+$values = mysqli_fetch_assoc(mysqli_query($link, $query));
 
-if(!empty($values['yearly_hub']))
-{
-  header("Location: index.php"); 
+if (!empty($values['yearly_hub'])) {
+  header("Location: index.php");
 }
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
+
+<head>
   <?php include('_head.php'); ?>
-  </head>
+</head>
 
-  <body>
+<body>
 
-    <?php include('_nav.php'); ?>
+  <?php include('_nav.php'); ?>
 
-    <div class="container">
-        <div class="row">
-            <h1 class="col-xs-12">Niyaaz takhmeen for full year from Shehrullah to Shabaan.</h1>
-            <h4 class="col-xs-12">Please select the total niyaaz amount that you will contribute for the this year.</h4>
-        </div>
-        <div class="row"><a href='selectyearlyhub_action.php?option=1' class="col-xs-12 col-sm-10 col-md-6 col-lg-4 btn btn-primary" style='margin:5px'>24,000/-</a></div>
-        <div class="row"><a href='selectyearlyhub_action.php?option=2' class="col-xs-12 col-sm-10 col-md-6 col-lg-4 btn btn-primary" style='margin:5px'>25,000/-</a></div>
-        <div class="row"><a href='selectyearlyhub_action.php?option=3' class="col-xs-12 col-sm-10 col-md-6 col-lg-4 btn btn-primary" style='margin:5px'>26,000/-</a></div>
-        <div class="row"><a href='selectyearlyhub_action.php?option=4' class="col-xs-12 col-sm-10 col-md-6 col-lg-4 btn btn-primary" style='margin:5px'>27,000/-</a></div>
-        <div class="row"><a href='selectyearlyhub_action.php?option=5' class="col-xs-12 col-sm-10 col-md-6 col-lg-4 btn btn-primary" style='margin:5px'>53,000/-</a></div>
+  <div class="container">
+    <div class="row">
+      <h1 class="col-xs-12">FMB takhmeen for full year from this Shehrullah to Shabaan.</h1>
+      <h4 class="col-xs-12">Please select the total niyaaz amount that you will contribute for the this year.</h4>
+    </div>
+    <div class="row"><a href='selectyearlyhub_action.php?option=1' class="col-xs-12 col-sm-10 col-md-6 col-lg-4 btn btn-primary" style='margin:5px'>30,000/-</a></div>
+    <div class="row"><a href='selectyearlyhub_action.php?option=2' class="col-xs-12 col-sm-10 col-md-6 col-lg-4 btn btn-primary" style='margin:5px'>35,000/-</a></div>
+    <div class="row"><a href='selectyearlyhub_action.php?option=3' class="col-xs-12 col-sm-10 col-md-6 col-lg-4 btn btn-primary" style='margin:5px'>40,000/-</a></div>
+    <div class="row"><a href='selectyearlyhub_action.php?option=4' class="col-xs-12 col-sm-10 col-md-6 col-lg-4 btn btn-primary" style='margin:5px'>53,000/-</a></div>
 
-        <div class="row">
-        <hr>
-         <div class="form-group">
-            <form method="post" action="selectyearlyhub_action.php">
-              <label for="inputEmail" class="col-lg control-label">Other ( > 24,000/-)</label>
-              <div class="col-lg">
-                <input type="number" class="form-control" name="other_takhmeen" min="24000"/>
-              </div>
-              <br>
-              <button type="submit" class="btn btn-primary">Save changes</button>
-            </form>
+    <div class="row">
+      <hr>
+      <div class="form-group">
+        <form method="post" action="selectyearlyhub_action.php">
+          <label for="inputEmail" class="col-lg control-label">Other ( > 30,000/-)</label>
+          <div class="col-lg">
+            <input type="number" class="form-control" name="other_takhmeen" min="24000" />
           </div>
-        </div>
-
-        
+          <br>
+          <button type="submit" class="btn btn-primary">Save changes</button>
+        </form>
+      </div>
     </div>
 
-    <?php include('_bottomJS.php'); ?>
-  
-    <div align="center">
+
+  </div>
+
+  <?php include('_bottomJS.php'); ?>
+
+  <div align="center">
     <a href="mailto:help@faizstudents.com">help@faizstudents.com</a><br><br>
-    </div>
-  </body>
+  </div>
+</body>
+
 </html>
--- users/selectyearlyhub_action.php
+++ users/selectyearlyhub_action.php
@@ -2,27 +2,26 @@
 include('connection.php');
 include('_authCheck.php');
 
-if(isset($_GET['option']))
-{
+if (isset($_GET['option'])) {
 	$huboptions = array(
-						1 => '24000',
-						2 => '25000',
-						3 => '26000',
-						4 => '27000',
-						5 => '53000'
-							);
+		1 => '30000',
+		2 => '35000',
+		3 => '40000',
+		4 => '53000'
+	);
 	$hub_amount = $huboptions[$_GET['option']];
-} else if(isset($_POST['other_takhmeen'])) {
+} else if (isset($_POST['other_takhmeen'])) {
 	$hub_amount = (int)$_POST['other_takhmeen'];
-	if($hub_amount < 24000) {
-		header("Location: selectyearlyhub.php?message=error"); exit;	
-	}	
+	if ($hub_amount < 30000) {
+		header("Location: selectyearlyhub.php?message=error");
+		exit;
+	}
 }
 
-if(isset($hub_amount)){
-	$thali_details = mysqli_fetch_assoc(mysqli_query($link,"SELECT musaid FROM thalilist WHERE Email_ID = '".$_SESSION['email']."'"));
+if (isset($hub_amount)) {
+	$thali_details = mysqli_fetch_assoc(mysqli_query($link, "SELECT musaid FROM thalilist WHERE Email_ID = '" . $_SESSION['email'] . "'"));
 	$musaid_name = !empty($thali_details['musaid']) ? $thali_details['musaid'] : 'mustafamnr@gmail.com';
-	$update = mysqli_query($link,"UPDATE thalilist set yearly_hub='$hub_amount', musaid='$musaid_name' WHERE Email_id = '".$_SESSION['email']."'") or die(mysqli_error($link));
+	$update = mysqli_query($link, "UPDATE thalilist set yearly_hub='$hub_amount', musaid='$musaid_name' WHERE Email_id = '" . $_SESSION['email'] . "'") or die(mysqli_error($link));
 }
 
 header("Location: index.php");
