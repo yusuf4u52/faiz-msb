925634b8d0c11aa10b0d4c47e674db78a5f73d4e
Yusuf Rampurawala <yusuf4u52@gmail.com>
2021-09-06 17:30:18 +0530

    receipt delete only available to YR as we need to take care of thalilist adjustments

--- admin/application/controllers/Examples.php
+++ admin/application/controllers/Examples.php
@@ -1,6 +1,7 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Examples extends CI_Controller {
+class Examples extends CI_Controller
+{
 
 	public function __construct()
 	{
@@ -13,7 +14,7 @@ class Examples extends CI_Controller {
 
 	public function _example_output($output = null)
 	{
-		$this->load->view('example.php',$output);
+		$this->load->view('example.php', $output);
 	}
 
 	public function offices()
@@ -24,158 +25,141 @@ class Examples extends CI_Controller {
 	}
 
 	function faiz()
-{
-	session_start();
-		if (!is_null($_SESSION['fromLogin']) && in_array($_SESSION['email'], array('nationalminerals52@gmail.com','mesaifee52@gmail.com','murtaza52@gmail.com','murtaza.sh@gmail.com','yusuf4u52@gmail.com','tzabuawala@gmail.com','mustafamnr@gmail.com')))
-		{
+	{
+		session_start();
+		if (!is_null($_SESSION['fromLogin']) && in_array($_SESSION['email'], array('nationalminerals52@gmail.com', 'mesaifee52@gmail.com', 'murtaza52@gmail.com', 'murtaza.sh@gmail.com', 'yusuf4u52@gmail.com', 'tzabuawala@gmail.com', 'mustafamnr@gmail.com'))) {
+		} else
+			header("Location: http://www.faizstudents.com/users/login.php");
 
-		}else
-		 header("Location: http://www.faizstudents.com/users/login.php");
+		$crud = new grocery_CRUD();
+		$crud->set_table('thalilist');
 
-    $crud = new grocery_CRUD();
-    $crud->set_table('thalilist');
+		$output = $crud->render();
 
-    $output = $crud->render();
- 
-    $this->_example_output($output);
-}
-
-function notpickedup()
-{
-	session_start();
-		if (!is_null($_SESSION['fromLogin']) && in_array($_SESSION['email'], array('mesaifee52@gmail.com','murtaza52@gmail.com','murtaza.sh@gmail.com','yusuf4u52@gmail.com','tzabuawala@gmail.com','mustafamnr@gmail.com')))
-		{
-
-		}else
-		 header("Location: http://www.faizstudents.com/users/login.php");
-
-    $crud = new grocery_CRUD();
-    $crud->set_table('not_picked_up');
-
-    $output = $crud->render();
- 
-    $this->_example_output($output);
-}
-
-function daily_hisab_items()
-{
-	session_start();
-		if (!is_null($_SESSION['fromLogin']) && in_array($_SESSION['email'], array('bscalcuttawala@gmail.com','mesaifee52@gmail.com','murtaza52@gmail.com','murtaza.sh@gmail.com','yusuf4u52@gmail.com','tzabuawala@gmail.com','mustafamnr@gmail.com')))
-		{
-
-		}else
-		 header("Location: http://www.faizstudents.com/users/login.php");
-
-    $crud = new grocery_CRUD();
-    $crud->set_table('daily_hisab_items');
-
-    $output = $crud->render();
- 
-    $this->_example_output($output);
-}
-
-function daily_menu_count()
-{
-	session_start();
-		if (!is_null($_SESSION['fromLogin']) && in_array($_SESSION['email'], array('bscalcuttawala@gmail.com','mesaifee52@gmail.com','murtaza52@gmail.com','murtaza.sh@gmail.com','yusuf4u52@gmail.com','tzabuawala@gmail.com','mustafamnr@gmail.com')))
-		{
-
-		}else
-		 header("Location: http://www.faizstudents.com/users/login.php");
-
-    $crud = new grocery_CRUD();
-    $crud->set_table('daily_hisab');
-
-    $output = $crud->render();
- 
-    $this->_example_output($output);
-}
-
-function sf_hisab()
-{
-	session_start();
-		if (!is_null($_SESSION['fromLogin']) && in_array($_SESSION['email'], array('bscalcuttawala@gmail.com','mesaifee52@gmail.com','murtaza52@gmail.com','murtaza.sh@gmail.com','yusuf4u52@gmail.com','tzabuawala@gmail.com','mustafamnr@gmail.com')))
-		{
-
-		}else
-		 header("Location: http://www.faizstudents.com/users/login.php");
-
-    $crud = new grocery_CRUD();
-    $crud->set_table('sf_hisab');
-
-    $output = $crud->render();
- 
-    $this->_example_output($output);
-}
-
-function receipts()
-{
-	session_start();
-		if (!is_null($_SESSION['fromLogin']) && in_array($_SESSION['email'], array('nationalminerals52@gmail.com','mesaifee52@gmail.com','bscalcuttawala@gmail.com','murtaza52@gmail.com','murtaza.sh@gmail.com','yusuf4u52@gmail.com','tzabuawala@gmail.com','mustafamnr@gmail.com')))
-		{
-
-		}else
-		 header("Location: http://www.faizstudents.com/users/login.php");
-
-    $crud = new grocery_CRUD();
-    $crud->set_table('receipts');
-    $crud->unset_edit();
-    if ($_SESSION['email'] =='bscalcuttawala@gmail.com')
-    {
-    $crud->unset_delete();
-	}
-    $crud->columns('Receipt_No','Thali_No','name','Amount','Date','received_by');
-
-    $output = $crud->render();
- 
-    $this->_example_output($output);
-}
-
-function change()
-{
-	session_start();
-	if (!is_null($_SESSION['fromLogin']) && in_array($_SESSION['email'], array('nationalminerals52@gmail.com','mesaifee52@gmail.com','murtaza52@gmail.com','murtaza.sh@gmail.com','yusuf4u52@gmail.com','tzabuawala@gmail.com','mustafamnr@gmail.com')))
-	{}
-	else {
-	 header("Location: http://www.faizstudents.com/users/login.php");
+		$this->_example_output($output);
 	}
 
-    $crud = new grocery_CRUD();
-    $crud->columns('id','Thali','Operation','Date','datetime');
-    $crud->set_table('change_table');
-    $crud->unset_edit();
-    $crud->unset_delete();
-    $crud->unset_add();
-    $output = $crud->render();
-    $this->_example_output($output);
-}
+	function notpickedup()
+	{
+		session_start();
+		if (!is_null($_SESSION['fromLogin']) && in_array($_SESSION['email'], array('mesaifee52@gmail.com', 'murtaza52@gmail.com', 'murtaza.sh@gmail.com', 'yusuf4u52@gmail.com', 'tzabuawala@gmail.com', 'mustafamnr@gmail.com'))) {
+		} else
+			header("Location: http://www.faizstudents.com/users/login.php");
 
-function event_response()
-{
-	session_start();
-		if (!is_null($_SESSION['fromLogin']) && in_array($_SESSION['email'], array('nationalminerals52@gmail.com','mesaifee52@gmail.com','murtaza52@gmail.com','murtaza.sh@gmail.com','yusuf4u52@gmail.com','tzabuawala@gmail.com','mustafamnr@gmail.com')))
-		{
+		$crud = new grocery_CRUD();
+		$crud->set_table('not_picked_up');
 
-		}else
-		 header("Location: http://www.faizstudents.com/users/login.php");
+		$output = $crud->render();
 
-    $crud = new grocery_CRUD();
-    $crud->set_table('event_response');
-    $crud->unset_edit();
-    $crud->unset_delete();
-    $output = $crud->render();
-    $this->_example_output($output);
-}
+		$this->_example_output($output);
+	}
+
+	function daily_hisab_items()
+	{
+		session_start();
+		if (!is_null($_SESSION['fromLogin']) && in_array($_SESSION['email'], array('bscalcuttawala@gmail.com', 'mesaifee52@gmail.com', 'murtaza52@gmail.com', 'murtaza.sh@gmail.com', 'yusuf4u52@gmail.com', 'tzabuawala@gmail.com', 'mustafamnr@gmail.com'))) {
+		} else
+			header("Location: http://www.faizstudents.com/users/login.php");
+
+		$crud = new grocery_CRUD();
+		$crud->set_table('daily_hisab_items');
+
+		$output = $crud->render();
+
+		$this->_example_output($output);
+	}
+
+	function daily_menu_count()
+	{
+		session_start();
+		if (!is_null($_SESSION['fromLogin']) && in_array($_SESSION['email'], array('bscalcuttawala@gmail.com', 'mesaifee52@gmail.com', 'murtaza52@gmail.com', 'murtaza.sh@gmail.com', 'yusuf4u52@gmail.com', 'tzabuawala@gmail.com', 'mustafamnr@gmail.com'))) {
+		} else
+			header("Location: http://www.faizstudents.com/users/login.php");
+
+		$crud = new grocery_CRUD();
+		$crud->set_table('daily_hisab');
+
+		$output = $crud->render();
+
+		$this->_example_output($output);
+	}
+
+	function sf_hisab()
+	{
+		session_start();
+		if (!is_null($_SESSION['fromLogin']) && in_array($_SESSION['email'], array('bscalcuttawala@gmail.com', 'mesaifee52@gmail.com', 'murtaza52@gmail.com', 'murtaza.sh@gmail.com', 'yusuf4u52@gmail.com', 'tzabuawala@gmail.com', 'mustafamnr@gmail.com'))) {
+		} else
+			header("Location: http://www.faizstudents.com/users/login.php");
+
+		$crud = new grocery_CRUD();
+		$crud->set_table('sf_hisab');
+
+		$output = $crud->render();
+
+		$this->_example_output($output);
+	}
+
+	function receipts()
+	{
+		session_start();
+		if (!is_null($_SESSION['fromLogin']) && in_array($_SESSION['email'], array('nationalminerals52@gmail.com', 'mesaifee52@gmail.com', 'bscalcuttawala@gmail.com', 'murtaza52@gmail.com', 'murtaza.sh@gmail.com', 'yusuf4u52@gmail.com', 'tzabuawala@gmail.com', 'mustafamnr@gmail.com'))) {
+		} else
+			header("Location: http://www.faizstudents.com/users/login.php");
+
+		$crud = new grocery_CRUD();
+		$crud->set_table('receipts');
+		$crud->unset_edit();
+		if (in_array($_SESSION['email'], array('nationalminerals52@gmail.com', 'mesaifee52@gmail.com', 'bscalcuttawala@gmail.com', 'murtaza52@gmail.com', 'murtaza.sh@gmail.com', 'tzabuawala@gmail.com', 'mustafamnr@gmail.com'))) {
+			$crud->unset_delete();
+		}
+		$crud->columns('Receipt_No', 'Thali_No', 'name', 'Amount', 'Date', 'received_by');
+
+		$output = $crud->render();
+
+		$this->_example_output($output);
+	}
+
+	function change()
+	{
+		session_start();
+		if (!is_null($_SESSION['fromLogin']) && in_array($_SESSION['email'], array('nationalminerals52@gmail.com', 'mesaifee52@gmail.com', 'murtaza52@gmail.com', 'murtaza.sh@gmail.com', 'yusuf4u52@gmail.com', 'tzabuawala@gmail.com', 'mustafamnr@gmail.com'))) {
+		} else {
+			header("Location: http://www.faizstudents.com/users/login.php");
+		}
+
+		$crud = new grocery_CRUD();
+		$crud->columns('id', 'Thali', 'Operation', 'Date', 'datetime');
+		$crud->set_table('change_table');
+		$crud->unset_edit();
+		$crud->unset_delete();
+		$crud->unset_add();
+		$output = $crud->render();
+		$this->_example_output($output);
+	}
+
+	function event_response()
+	{
+		session_start();
+		if (!is_null($_SESSION['fromLogin']) && in_array($_SESSION['email'], array('nationalminerals52@gmail.com', 'mesaifee52@gmail.com', 'murtaza52@gmail.com', 'murtaza.sh@gmail.com', 'yusuf4u52@gmail.com', 'tzabuawala@gmail.com', 'mustafamnr@gmail.com'))) {
+		} else
+			header("Location: http://www.faizstudents.com/users/login.php");
+
+		$crud = new grocery_CRUD();
+		$crud->set_table('event_response');
+		$crud->unset_edit();
+		$crud->unset_delete();
+		$output = $crud->render();
+		$this->_example_output($output);
+	}
 
 	public function index()
 	{
-		$this->_example_output((object)array('output' => '' , 'js_files' => array() , 'css_files' => array()));
+		$this->_example_output((object)array('output' => '', 'js_files' => array(), 'css_files' => array()));
 	}
 
-		
+
 	public function valueToEuro($value, $row)
 	{
-		return $value.' &euro;';
+		return $value . ' &euro;';
 	}
-			
 }
