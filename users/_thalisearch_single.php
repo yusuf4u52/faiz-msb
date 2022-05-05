c2f5cb974ee815bd6ee4428743cd6f520be6651e
Yusuf Rampurawala <yusuf4u52@gmail.com>
2022-05-05 19:45:06 +0530

    Thali search pending hoob table

--- users/_thalisearch_single.php
+++ users/_thalisearch_single.php
@@ -2,10 +2,10 @@
 $values = mysqli_fetch_assoc($result);
 
 $comments_query = "SELECT `comments`.*, `thalilist`.`NAME` FROM `comments` INNER JOIN `thalilist` on `comments`.`author_id` = `thalilist`.`id`
-WHERE `comments`.`user_id` = '".$values['id']."' ORDER BY `comments`.`created` DESC ";
-$comments_result = mysqli_query($link,$comments_query);
+WHERE `comments`.`user_id` = '" . $values['id'] . "' ORDER BY `comments`.`created` DESC ";
+$comments_result = mysqli_query($link, $comments_query);
 
-$musaid_details = mysqli_fetch_assoc(mysqli_query($link,"SELECT NAME, CONTACT FROM thalilist where Email_id = '".$values['musaid']."'"));
+$musaid_details = mysqli_fetch_assoc(mysqli_query($link, "SELECT NAME, CONTACT FROM thalilist where Email_id = '" . $values['musaid'] . "'"));
 
 ?>
 <div class="panel panel-default">
@@ -13,90 +13,109 @@ $musaid_details = mysqli_fetch_assoc(mysqli_query($link,"SELECT NAME, CONTACT FR
     <div class="panel-body">
       <ul class="list-group col">
         <li class="list-group-item">
-          <a href="#" data-key="payhoob" data-thali="<?php echo $values['Thali']; ?>">Pay Hoob</a> | 
+          <a href="#" data-key="payhoob" data-thali="<?php echo $values['Thali']; ?>">Pay Hoob</a> |
           <?php
-        if($values['Active'] == '1') {?>
-          <a href="#" data-key="stopthaali" data-thali="<?php echo $values['Thali']; ?>" data-active="0">Stop Thaali</a> | 
-          <?php }else{ ?>
-          <a href="#" data-key="stopthaali" data-thali="<?php echo $values['Thali']; ?>" data-active="1">Start Thaali</a> | 
-          <?php } 
-          if($values['Active'] != '2') {?>
-          <a href="#" data-key="stoppermanant" data-thali="<?php echo $values['Thali']; ?>">Stop Permanent</a> |
-        <?php } ?>
-        <a data-toggle="modal" href="#changeMusaid">Change Musaid</a>
+          if ($values['Active'] == '1') { ?>
+            <a href="#" data-key="stopthaali" data-thali="<?php echo $values['Thali']; ?>" data-active="0">Stop Thaali</a> |
+          <?php } else { ?>
+            <a href="#" data-key="stopthaali" data-thali="<?php echo $values['Thali']; ?>" data-active="1">Start Thaali</a> |
+          <?php }
+          if ($values['Active'] != '2') { ?>
+            <a href="#" data-key="stoppermanant" data-thali="<?php echo $values['Thali']; ?>">Stop Permanent</a> |
+          <?php } ?>
+          <a data-toggle="modal" href="#changeMusaid">Change Musaid</a>
         </li>
         <li class="list-group-item">
-            <h6 class="list-group-item-head ing text-muted">Thaali Number</h6>
-            <p class="list-group-item-text"><strong><?php echo $values['Thali']; ?></strong></p>
+          <h6 class="list-group-item-head ing text-muted">Thaali Number</h6>
+          <p class="list-group-item-text"><strong><?php echo $values['Thali']; ?></strong></p>
         </li>
         <li class="list-group-item">
-            <h6 class="list-group-item-head ing text-muted">Name</h6>
-            <p class="list-group-item-text"><strong><?php echo $values['NAME']; ?></strong></p>
+          <h6 class="list-group-item-head ing text-muted">Name</h6>
+          <p class="list-group-item-text"><strong><?php echo $values['NAME']; ?></strong></p>
         </li>
         <li class="list-group-item">
-            <h6 class="list-group-item-head ing text-muted">Mobile No</h6>
-            <p class="list-group-item-text"><strong><?php echo $values['CONTACT']; ?></strong></p>
+          <h6 class="list-group-item-head ing text-muted">Mobile No</h6>
+          <p class="list-group-item-text"><strong><?php echo $values['CONTACT']; ?></strong></p>
         </li>
-        <?php if($musaid_details) { ?>
-        <li class="list-group-item">
+        <?php if ($musaid_details) { ?>
+          <li class="list-group-item">
             <h6 class="list-group-item-head ing text-muted">Musaid</h6>
             <p class="list-group-item-text"><strong><?php echo $musaid_details['NAME']; ?> | <a href="tel:<?php echo $musaid_details['CONTACT']; ?>"><?php echo $musaid_details['CONTACT']; ?></a></strong></p>
-        </li>
-        <?php } ?>         
+          </li>
+        <?php } ?>
         <li class="list-group-item">
-            <h6 class="list-group-item-head ing text-muted">Active</h6>
-            <p class="list-group-item-text"><strong><?php echo ($values['Active'] == '1') ? 'Yes' : 'No'; ?></strong></p>
-        </li>
-        <li class="list-group-item">
-            <h6 class="list-group-item-head ing text-muted">Transporter</h6>
-            <p class="list-group-item-text"><strong><?php echo $values['Transporter']; ?></strong></p>
+          <h6 class="list-group-item-head ing text-muted">Active</h6>
+          <p class="list-group-item-text"><strong><?php echo ($values['Active'] == '1') ? 'Yes' : 'No'; ?></strong></p>
         </li>
         <li class="list-group-item">
-            <h6 class="list-group-item-head ing text-muted">Address</h6>
-            <p class="list-group-item-text"><strong><?php echo $values['Full_Address']; ?></strong></p>
+          <h6 class="list-group-item-head ing text-muted">Transporter</h6>
+          <p class="list-group-item-text"><strong><?php echo $values['Transporter']; ?></strong></p>
         </li>
-
         <li class="list-group-item">
-            <h6 class="list-group-item-head ing text-muted">Start Date</h6>
-            <p class="list-group-item-text"><strong class="hijridate"><?php echo $values['Thali_start_date']; ?></strong></p>
+          <h6 class="list-group-item-head ing text-muted">Address</h6>
+          <p class="list-group-item-text"><strong><?php echo $values['Full_Address']; ?></strong></p>
         </li>
 
         <li class="list-group-item">
-            <h6 class="list-group-item-head ing text-muted">Stop Date</h6>
-            <p class="list-group-item-text"><strong class="hijridate"><?php echo $values['Thali_stop_date']; ?></strong></p>
+          <h6 class="list-group-item-head ing text-muted">Start Date</h6>
+          <p class="list-group-item-text"><strong class="hijridate"><?php echo $values['Thali_start_date']; ?></strong></p>
         </li>
 
         <li class="list-group-item">
-            <h6 class="list-group-item-head ing text-muted">Hub Pending</h6>
-            <p class="list-group-item-text"><strong><?php echo $values['Total_Pending'] + $values['Paid']; ?> - <?php echo $values['Paid']; ?> = <?php echo $values['Total_Pending']; ?></strong></p>
+          <h6 class="list-group-item-head ing text-muted">Stop Date</h6>
+          <p class="list-group-item-text"><strong class="hijridate"><?php echo $values['Thali_stop_date']; ?></strong></p>
         </li>
 
         <li class="list-group-item">
-            <h6 class="list-group-item-head ing text-muted">Thali Delivered</h6>
-            <p class="list-group-item-text"><strong><?php echo round($values['thalicount'] * 100 / $max_days[0]); ?>% of days</strong></p>
+          <h5 class="list-group-item-heading text-muted">Pending Hoob</h5>
+          <ul class="list-group">
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+              Previous Year Pending
+              <span class="badge bg-primary rounded-pill"><?php echo $values['Previous_Due']; ?></span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+              Current Year Takhmeen
+              <span class="badge bg-primary rounded-pill">+ <?php echo $values['yearly_hub']; ?></span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+              Paid
+              <span class="badge bg-primary rounded-pill"><a href="hoobHistory.php">- <?php echo $values['Paid']; ?></a></span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+              Total Pending
+              <span class="badge bg-primary rounded-pill"><?php echo $values['Total_Pending']; ?></span>
+            </li>
+          </ul>
+        </li>
+
+        <li class="list-group-item">
+          <h6 class="list-group-item-head ing text-muted">Thali Delivered</h6>
+          <p class="list-group-item-text"><strong><?php echo round($values['thalicount'] * 100 / $max_days[0]); ?>% of days</strong></p>
         </li>
         <li class="list-group-item">
           <table class="table table-striped table-hover">
-          <tr>
-          <td><b>Receipt No</b></td>
-          <td><b>Amount</b></td>
-          <td><b>Date</b></td>
-          </tr>
-          <?php
-          $query = "SELECT r.* FROM $receipts_tablename r, thalilist t WHERE r.userid = t.id and t.id ='".$values['id']."' ORDER BY Date ASC";
-          $result = mysqli_query($link,$query);
-          while($row = mysqli_fetch_assoc($result)){ 
-              foreach($row AS $key => $value) { $row[$key] = stripslashes($value); } 
-              echo "<tr>";  
-              echo "<td>" . nl2br( $row['Receipt_No']) . "</td>";  
-              echo "<td>" . nl2br( $row['Amount']) . "</td>";  
-              echo "<td class=\"hijridate\">" . nl2br( $row['Date']) . "</td>";  
-              echo "</tr>"; 
-          } 
-          ?>
+            <tr>
+              <td><b>Receipt No</b></td>
+              <td><b>Amount</b></td>
+              <td><b>Date</b></td>
+            </tr>
+            <?php
+            $query = "SELECT r.* FROM $receipts_tablename r, thalilist t WHERE r.userid = t.id and t.id ='" . $values['id'] . "' ORDER BY Date ASC";
+            $result = mysqli_query($link, $query);
+            while ($row = mysqli_fetch_assoc($result)) {
+              foreach ($row as $key => $value) {
+                $row[$key] = stripslashes($value);
+              }
+              echo "<tr>";
+              echo "<td>" . nl2br($row['Receipt_No']) . "</td>";
+              echo "<td>" . nl2br($row['Amount']) . "</td>";
+              echo "<td class=\"hijridate\">" . nl2br($row['Date']) . "</td>";
+              echo "</tr>";
+            }
+            ?>
           </table>
         </li>
-      </ul>  
+      </ul>
 
       <!-- Comment section starts-->
       <div class="row bootstrap snippets">
@@ -117,18 +136,18 @@ $musaid_details = mysqli_fetch_assoc(mysqli_query($link,"SELECT NAME, CONTACT FR
                 <hr>
                 <ul class="media-list">
 
-                <?php
-                  while($comment = mysqli_fetch_assoc($comments_result)) {
-                ?>
-                  <li class="media">
-                    <div class="media-body">
-                      <span class="text-muted pull-right">
-                        <small class="text-muted"><?php echo $comment['created']; ?></small>
-                      </span>
-                      <strong class="text-success"><?php echo $comment['NAME']; ?></strong>
-                      <p><?php echo $comment['comment']; ?></p>
-                    </div>
-                  </li>
+                  <?php
+                  while ($comment = mysqli_fetch_assoc($comments_result)) {
+                  ?>
+                    <li class="media">
+                      <div class="media-body">
+                        <span class="text-muted pull-right">
+                          <small class="text-muted"><?php echo $comment['created']; ?></small>
+                        </span>
+                        <strong class="text-success"><?php echo $comment['NAME']; ?></strong>
+                        <p><?php echo $comment['comment']; ?></p>
+                      </div>
+                    </li>
 
                   <?php
                   }
@@ -141,6 +160,6 @@ $musaid_details = mysqli_fetch_assoc(mysqli_query($link,"SELECT NAME, CONTACT FR
           </div>
           <!-- Comment section ends-->
 
+        </div>
+      </div>
     </div>
-  </div>
-  </div>
