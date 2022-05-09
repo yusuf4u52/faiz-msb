dcfb0c357c9f1f66fd04e9b10a7f7c21693e9d9e
Yusuf Rampurawala <yusuf4u52@gmail.com>
2022-05-09 20:59:10 +0530

    user details endpoint reuse

--- users/event_response.php
+++ users/event_response.php
@@ -1,8 +1,16 @@
 <?php
 include('connection.php');
+if ($faiz_api_key != $_POST['api_key']) {
+    header("Location: login.php");
+    exit;
+}
 $query = "Select * from thalilist where id='".$_POST['Thaliid']."'";
 $result= mysqli_query($link,$query);
 $values = mysqli_fetch_assoc($result);
 
-mysqli_query($link, "INSERT INTO event_response (`thaliid`,`thalino`,`eventid`,`response`,`its`,`name`,`mobile`,`comments`) VALUES ('".$_POST['Thaliid']."','".$values['Thali']."','".$_POST['Eventid']."','".$_POST['Response']."','".$values['ITS_No']."','".$values['NAME']."','".$values['CONTACT']."','".$_POST['Comments']."')") or die(mysqli_error($link));
+print_r(json_encode($values));
+
+if (isset($_POST['Response'])) {
+    mysqli_query($link, "INSERT INTO event_response (`thaliid`,`thalino`,`eventid`,`response`,`its`,`name`,`mobile`,`comments`) VALUES ('".$_POST['Thaliid']."','".$values['Thali']."','".$_POST['Eventid']."','".$_POST['Response']."','".$values['ITS_No']."','".$values['NAME']."','".$values['CONTACT']."','".$_POST['Comments']."')") or die(mysqli_error($link));
+}
 ?>
