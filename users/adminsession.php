36944a23da5e692f8a2c010c39d946bcbbc7b8fe
Yusuf Rampurawala <yusuf4u52@gmail.com>
2022-10-16 11:00:16 +0530

    remove + sign

--- users/adminsession.php
+++ users/adminsession.php
@@ -3,6 +3,7 @@ session_start();
 
 if (!is_null($_SESSION['fromLogin']) && in_array($_SESSION['email'], array('mesaifee52@gmail.com','husainpoonawala1995@gmail.com','nationalminerals52@gmail.com','murtaza52@gmail.com','yusuf4u52@gmail.com','tzabuawala@gmail.com','hzfshakir199@gmail.com','murtaza.sh@gmail.com','mustafamnr@gmail.com', 'ismailsidhpuri@gmail.com'))) {
 } elseif (!empty($_POST['mobile'])) {
+  $_POST['mobile'] = str_replace('+', '', $_POST['mobile']); 
 	// set email based on mobile number
   $sql = mysqli_query($link,"SELECT email from users where mobile='".$_POST['mobile']."'");
   $row = mysqli_fetch_row($sql);
